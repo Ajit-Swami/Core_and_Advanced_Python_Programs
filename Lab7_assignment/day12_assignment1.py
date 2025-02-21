7797fad37ac1173a6755ce2375b103be34ee3e3e
Ajit_Swami_189 <161943229+Ajit-Swami@users.noreply.github.com>
2025-02-21 12:34:23 +0530

    Add files via upload

---
+++ Lab7_assignment/day12_assignment1.py
@@ -0,0 +1,8 @@
+tuplex = (2, 4, 5, 6, 2, 3, 4, 4, 7)
+count_4 = tuplex.count(4)
+print("Output:", count_4)
+
+
+'''
+Output: 3
+'''
---
+++ Lab7_assignment/day12_assignment2.py
@@ -0,0 +1,8 @@
+listx = [5, 10, 7, 4, 15, 3]
+tuplex = tuple(listx)
+print("Output:", tuplex)
+
+
+'''
+Output: (5, 10, 7, 4, 15, 3)
+'''
